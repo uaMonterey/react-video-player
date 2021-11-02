834f0f342fc4aa874e28cb5ff01d8c27504d751c
Sergii Maksymenko <sergiomaksymenko@gmail.com>
2021-11-02 16:17:07 +0100

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,8 @@
 import { Component } from 'react';
-import ReactPlayer from 'react-player';
 import { Container } from './App.styled';
 import { VideoList } from 'components/VideoList/VideoList';
+import { Player } from 'components/Player/Player';
+
 import videos from 'data/videos.json';
 
 export class App extends Component {
@@ -15,11 +16,12 @@ export class App extends Component {
 
   render() {
     const { selectedVideo } = this.state;
+
     return (
       <Container>
         <h1>Selected Video: {selectedVideo}</h1>
         <VideoList videos={videos} onSelect={this.selectedVideo} />
-        {selectedVideo && <ReactPlayer url={selectedVideo} controls />}
+        <Player url={selectedVideo} />
       </Container>
     );
   }
